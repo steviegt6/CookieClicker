0fd0b46aee212968869229fc0b841540bce5f66a
Stevie <xxlennygamerxx@gmail.com>
2020-09-04 01:44:39 -0700

    Commit numero uno

---
+++ CookieClicker.cs
@@ -0,0 +1,8 @@
+using Terraria.ModLoader;
+
+namespace CookieClicker
+{
+	public class CookieClicker : Mod
+	{
+	}
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# CookieClicker
+ Cookie Clicker mod for Terraria 1.3.5.3, using tModLoader 0.11.7.5!
